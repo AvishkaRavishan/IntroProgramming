db21b892141efb1e542a1994b05c93eb448898f3
Avishka Ravishan <101692241+AvishkaRavishan@users.noreply.github.com>
2025-02-09 16:56:48 +1100

    Update q11.py

--- q11.py
+++ q11.py
@@ -2,5 +2,8 @@ def sum_even_numbers(numbers):
   total=0
   for num in numbers:
     if num % 2 ==0:
-      total =+ num
+      total += num
   return total
+
+nums=[1,10,3,4,5,9]
+print("ans:",sum_even_numbers(nums))
